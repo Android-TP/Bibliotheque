13ed387c565f7e4508846e95d417fab0bf348920
Daniel Assani <devasddaniel@gmail.com>
2022-12-13 14:23:01 +0200

    authenticate sanctum

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,12 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\Route;
+use App\Models\Commande;
+use App\Models\Emprunt;
+use App\Models\Abonnement;
+use App\Models\Detail_user;
+use App\Models\Role;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -26,5 +32,26 @@ class AppServiceProvider extends ServiceProvider
     {
         //
         Schema::defaultStringLength(191);
+
+        Route::bind('Commande', function($value){
+            return Commande::findOrFail($value);
+        });
+
+        Route::bind('Emprunt', function($value){
+            return Emprunt::findOrFail($value);
+        });
+
+        Route::bind('Abonnement', function($value){
+            return Abonnement::findOrFail($value);
+        });
+
+        Route::bind('Detail_user', function($value){
+            return Detail_user::findOrFail($value);
+        });
+
+        Route::bind('Role', function($value){
+            return Role::findOrFail($value);
+        });
+
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,10 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Abonnement\AbonnementController;
+use App\Http\Controllers\User\DetailUserController;
+use App\Http\Controllers\User\RoleController;
+use App\Http\Controllers\EmpruntController;
+use App\Http\Controllers\CommandeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,9 +23,16 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::controller(AbonnementController::class)->group(function () {
-    Route::post("/abonnement", "store");
-    Route::get("/abonnement",'index');
-    Route::get("/abonnement/{id}", 'show')->where('id', '[0-9]+');
-});
+// Route::controller(AbonnementController::class)->group(function () {
+//     Route::post("/abonnement", "store");
+//     Route::get("/abonnement",'index');
+//     Route::get("/abonnement/{id}", 'show')->where('id', '[0-9]+');
+// });
+Route::middleware("auth:sanctum")->group(function () {
+Route::resource('commande', CommandeController::class)->except(["create", "edit"]);
+Route::resource('abonnement', AbonnementController::class)->except(["create", "edit"]);
+Route::resource('detail_user', DetailUserController::class)->except(["create", "edit"]);
+Route::resource('emprunt', EmpruntController::class)->except(["create", "edit"]);
+Route::resource('role', RoleController::class)->except(["create", "edit"]);
 
+});
