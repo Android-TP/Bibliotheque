54a5fe55b9047591bc3d3545e337d01b9a870758
Daniel Assani <devasddaniel@gmail.com>
2022-12-15 10:38:18 +0200

    add abonnement color

--- resources/views/components/navbars/sidebar.blade.php
+++ resources/views/components/navbars/sidebar.blade.php
@@ -1,7 +1,7 @@
 @props(['activePage'])
 
 <aside
-    class="sidenav navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 fixed-start ms-3   bg-gradient-dark"
+    class="sidenav navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 fixed-start ms-3    royal-blue"
     id="sidenav-main">
     <div class="sidenav-header">
         <i class="fas fa-times p-3 cursor-pointer text-white opacity-5 position-absolute end-0 top-0 d-none d-xl-none"
@@ -24,16 +24,16 @@
     </div>
     <div class="sidenav-footer position-absolute w-100 bottom-0 ">
         <div class="mx-3">
-            <a class="btn bg-gradient-primary w-100" href="{{route('abonnement')}}" >Abonnement</a>
+            <a class="btn bg-red w-100" href="{{route('abonnement')}}" >Abonnement</a>
         </div>
         <div class="mx-3">
-            <a class="btn bg-gradient-primary w-100" href="{{route('commander')}}" >Commandes</a>
+            <a class="btn bg-red w-100" href="{{route('commander')}}" >Commandes</a>
         </div>
         <div class="mx-3">
-            <a class="btn bg-gradient-primary w-100" href="{{route('books')}}" >Livres</a>
+            <a class="btn bg-red w-100" href="{{route('books')}}" >Livres</a>
         </div>
         <div class="mx-3">
-            <a class="btn bg-gradient-primary w-100"
+            <a class="btn bg-red w-100"
                 href="https://www.creative-tim.com/product/material-dashboard-pro-laravel" target="_blank" type="button">Emprunts</a>
         </div>
     </div>
--- resources/views/pages/abonnement.blade.php
+++ resources/views/pages/abonnement.blade.php
@@ -3,9 +3,17 @@
     <x-navbars.sidebar activePage="billing"></x-navbars.sidebar>
     <main class="main-content position-relative max-height-vh-100 h-100 border-radius-lg ">
         <!-- Navbar -->
-        <x-navbars.navs.auth titlePage="Livres"></x-navbars.navs.auth>
+        <x-navbars.navs.auth titlePage="Abonnement"></x-navbars.navs.auth>
 
-
+     <div class="container">
+        <div class="card">
+            <div class="card-header royal-blue color-white" >Abonnement</div>
+  <div class="card-body">Année : 2022-2023</div>
+  <div class="card-footer">
+    <div class="btn bg-red">S'abonner</div>
+  </div>
+        </div>
+     </div>
 
     </main>
     <x-plugins></x-plugins>
